e0cd048d710809f8a52f424bad0ed9430a9d984c
Felipe Bleson <felipe_bleson@bitbucket.org>
2014-11-29 15:44:28 -0200

    Criado os Repositorios que armazenam dados em xlm

---
+++ Data/Servico/RepositorioAccont.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using Inovix.Data.Fabrica;
+
+namespace Inovix.Data.Servico
+{
+    public class RepositorioAccont
+    {
+        public const string NomeArquivo = "D://DadosAccount.xml";
+
+        public RepositorioAccont()
+        {
+            if (File.Exists(NomeArquivo))
+            {
+                File.Delete(NomeArquivo);
+            }
+
+            Salvar(FabricaAccount.ListaAccounts().ToList());   
+        }
+
+        public static List<Account> RetornaUsuarios()
+        {
+            var accounts = new List<Account>();
+            var ser = new XmlSerializer(typeof(List<Account>));
+            var fs = new FileStream(NomeArquivo, FileMode.OpenOrCreate);
+            try
+            {
+                accounts = ser.Deserialize(fs) as List<Account>;
+            }
+            catch (InvalidOperationException)
+            {
+                if (accounts != null)
+                    ser.Serialize(fs, accounts);
+            }
+            finally
+            {
+                fs.Close();
+            }
+            return accounts;
+        }
+
+        private static void Salvar(List<Account> accounts)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(List<Account>));
+            FileStream fs = new FileStream(NomeArquivo, FileMode.OpenOrCreate);
+            ser.Serialize(fs, accounts);
+            fs.Close();
+        }
+
+        public static void Inserir(Account customer)
+        {
+            List<Account> listuUsuarios = RetornaUsuarios();
+            int id = 1;
+
+            if (listuUsuarios.Any())
+            {
+                id = id + listuUsuarios.Max(u => u.IdAccount);
+            }
+
+            customer.IdAccount = id;
+
+            listuUsuarios.Add(customer);
+            Salvar(listuUsuarios);
+        }
+
+        public static void Editar(Account customer)
+        {
+            List<Account> accounts = RetornaUsuarios().Select(item => item.IdAccount == customer.IdAccount ? customer : item).ToList();
+
+            Salvar(accounts);
+        }
+
+        public static void Deletar(int id)
+        {
+            List<Account> accounts = RetornaUsuarios().ToList();
+            accounts.Remove(accounts.First(u => u.IdAccount == id));
+            Salvar(accounts);
+        }
+    }
+}
---
+++ Data/Servico/RepositorioCustomer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Inovix.Data.Fabrica;
+
+namespace Inovix.Data.Servico
+{
+    public class RepositorioCustomer
+    {
+        public const string NomeArquivo = "D://DadosCustomer.xml";
+
+        public RepositorioCustomer()
+        {
+            if (File.Exists(NomeArquivo))
+            {
+                File.Delete(NomeArquivo);
+            }
+
+            Salvar(FabricaCustomer.ListaCustomers().ToList());   
+        }
+
+        public static List<Customer> RetornaUsuarios()
+        {
+            var usuarios = new List<Customer>();
+            var ser = new XmlSerializer(typeof(List<Customer>));
+            var fs = new FileStream(NomeArquivo, FileMode.OpenOrCreate);
+            try
+            {
+                usuarios = ser.Deserialize(fs) as List<Customer>;
+            }
+            catch (InvalidOperationException ex)
+            {
+                if (usuarios != null)
+                    ser.Serialize(fs, usuarios);
+            }
+            finally
+            {
+                fs.Close();
+            }
+            return usuarios;
+        }
+
+        private static void Salvar(List<Customer> usuarios)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(List<Customer>));
+            FileStream fs = new FileStream(NomeArquivo, FileMode.OpenOrCreate);
+            ser.Serialize(fs, usuarios);
+            fs.Close();
+        }
+
+        public static void Inserir(Customer customer)
+        {
+            List<Customer> listuUsuarios = RetornaUsuarios();
+            int id = 1;
+
+            if (listuUsuarios.Any())
+            {
+                id = id + listuUsuarios.Max(u => u.IdCustomer);
+            }
+
+            customer.IdCustomer = id;
+
+            listuUsuarios.Add(customer);
+            Salvar(listuUsuarios);
+        }
+
+        public static void Editar(Customer customer)
+        {
+            List<Customer> usuarios = RetornaUsuarios().Select(item => item.IdCustomer == customer.IdCustomer ? customer : item).ToList();
+
+            Salvar(usuarios);
+        }
+
+        public static void Deletar(int id)
+        {
+            List<Customer> usuarios = RetornaUsuarios().ToList();
+            usuarios.Remove(usuarios.First(u => u.IdCustomer == id));
+            Salvar(usuarios);
+        }
+    }
+}
