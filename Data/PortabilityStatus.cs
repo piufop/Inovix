5d5f6e0be1fdbdfcebdd3086d1741b31d0efe801
Hugo Machado <piufop@gmail.com>
2014-11-30 16:04:12 -0200

    Implementação do APT
    Criados os métodos na interface e a classe do PortabilityTicket.

---
+++ Data/PortabilityStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inovix.Data
+{
+    public enum PortabilityStatus
+    {
+        Success = 0,
+        Failed = 1
+    }
+}
---
+++ Data/PortabilityTicket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Inovix.Data
+{
+    [Serializable]
+    [DataContract]
+    public class PortabilityTicket
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        public PortabilityStatus Status { get; set; }
+    }
+}
